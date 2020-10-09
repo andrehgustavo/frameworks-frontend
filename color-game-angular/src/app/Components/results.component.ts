efda06c646252af26fe1c820fa1847e4ee099cf2
andrehgustavo <andreh_gustavo@ufrn.edu.br>
2020-10-09 19:45:01 -0300

    Transferindo pasta componentes para dentro da App

---
+++ color-game-angular/src/app/Components/results.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core'
+
+@Component({
+  selector: 'results',
+  template:
+    `
+  <div>
+    <h2>GameOver</h2>
+    <h3>
+        The right answer is:
+        <span
+            style="color: {{rightColor.colorNumber}}">{{rightColor.colorName}}</span>
+    </h3>
+    <h2>Hits: {{playerHits}}</h2>
+    </div>
+    <div>
+      <button class="colorful-button" (click)="refreshPage()">Restart Game</button>
+    </div>
+  `
+})
+export class ResultsComponent {
+  @Input() rightColor: {
+    colorNumber: string;
+    colorName: string;
+  };
+  @Input() playerHits: number
+
+  refreshPage() {
+    location.reload();
+  }
+
+}
--- color-game-angular/src/app/app.module.ts
+++ color-game-angular/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { GameComponent } from '../Components/game.component';
-import { ScreenComponent } from '../Components/screen.component';
-import { ResultsComponent } from '../Components/results.component';
-import { TimerComponent } from "../Components/timer.components";
-import { HomeComponent } from "../Components/home.component";
+import { GameComponent } from './Components/game.component';
+import { ScreenComponent } from './Components/screen.component';
+import { ResultsComponent } from './Components/results.component';
+import { TimerComponent } from "./Components/timer.components";
+import { HomeComponent } from "./Components/home.component";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, GameComponent, ScreenComponent, ResultsComponent, TimerComponent],
