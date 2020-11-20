2b8e6c93f1c9c19b4a8f365b6577d7d2df6e55e5
andrehgustavo <andreh_gustavo@ufrn.edu.br>
2020-11-20 18:35:05 -0300

    Padronizando svelte.

--- color-game-svelte/src/components/validations.js
+++ color-game-svelte/src/components/validations.js
@@ -1,13 +1,13 @@
 export function minLengthValidation(minLength, value) {
     if (!value || value.trim().length < minLength) {
-      return `Este campo requer pelo menos ${minLength} caracteres   `
+      return `This field requires at least ${minLength} characters   `
     }
     return null
   }
   
   export function requiredValidation(value) {
     if (!value || value.trim() == '') {
-      return 'Este campo é obrigatório'
+      return 'Required field'
     }
     return null
   }
